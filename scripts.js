27ef4933e9cc3cf531412020e9095adc37afbdae
adamkuylenstierna <adam.kuylenstierna@funnel.io>
2023-05-26 23:16:17 +0200

    new update

--- scripts.js
+++ scripts.js
@@ -1,60 +1,60 @@
 let tweetIndex = 0;
 let preloadedImages = [];
+const totalTweets = 23;
 
-// Total number of tweets
-const totalTweets = 17;
-
-// Generate list of tweet images
 const tweets = [];
+
+// Generate list of tweet numbers
 for (let i = 1; i <= totalTweets; i++) {
-    tweets.push(`images/tweet${i}.png`);
+  tweets.push(i);
 }
 
-// Function to preload all images
+// Function to shuffle the tweet numbers
+function shuffleTweets() {
+  for (let i = tweets.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [tweets[i], tweets[j]] = [tweets[j], tweets[i]];
+  }
+}
+
+// Preload all images
 function preloadImages(imageArray, callback) {
-    let i, j, loaded = 0;
-    const images = [];
-    for (i = 0, j = imageArray.length; i < j; i++) {
-        images[i] = new Image();
-        images[i].src = imageArray[i];
-        images[i].onload = function() {
-            if (++loaded === imageArray.length) {
-                callback(images);
-            }
-        };
-    }
+  let loadedCount = 0;
+
+  imageArray.forEach((imageSrc) => {
+    const image = new Image();
+    image.src = `images/tweet${imageSrc}.png`;
+    image.onload = () => {
+      loadedCount++;
+      if (loadedCount === imageArray.length) {
+        callback();
+      }
+    };
+
+    preloadedImages.push(image);
+  });
 }
 
 // Function to add a new tweet at the top of the container
 function addTweet() {
-    const tweetContainer = document.getElementById('tweetContainer');
-    
-    // Create a new img element for the tweet
-    const tweetImage = preloadedImages[tweetIndex];
-    tweetImage.className = 'tweetImage';
-    tweetImage.style.transform = 'translateY(-100%)'; // Set initial position above the container
-    
-    // Add the new tweet at the top of the container
-    tweetContainer.prepend(tweetImage);
-    
-    // Scroll smoothly to the new tweet
-    tweetContainer.scrollTop = 0;
-    
-    // Apply sliding animation to the tweet
-    setTimeout(() => {
-        tweetImage.style.transform = 'translateY(0%)';
-    }, 100);
-    
-    // Load the next tweet randomly
-    tweetIndex = Math.floor(Math.random() * preloadedImages.length);
-    
-    // Load the next tweet after a delay
-    setTimeout(addTweet, 5000);
+  const tweetContainer = document.getElementById('tweetContainer');
+
+  const tweetImage = preloadedImages[tweets[tweetIndex] - 1];
+  tweetImage.className = 'tweetImage';
+  tweetContainer.prepend(tweetImage);
+
+  tweetIndex++;
+  if (tweetIndex >= totalTweets) {
+    // Shuffle the tweet numbers for the next session
+    shuffleTweets();
+    tweetIndex = 0;
+  }
+
+  setTimeout(addTweet, 7000);
 }
 
-// Preload all images and then start adding tweets
-preloadImages(tweets, function(images) {
-    preloadedImages = images;
-    // Add the first tweet right away
-    addTweet();
+// Preload images and start adding tweets
+preloadImages(tweets, function () {
+  shuffleTweets(); // Shuffle the tweet numbers initially
+  addTweet();
 });
